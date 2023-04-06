9527a1045297a97853d6a5cb722a0095bc6fa766
vino <vino0908@outlook.com>
2023-04-06 09:51:16 +0800

    chore: 打包 docker

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const UnoCSS = require('@unocss/webpack').default
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output:'standalone',
   reactStrictMode: true,
   webpack: (config) => {
     config.cache = false
